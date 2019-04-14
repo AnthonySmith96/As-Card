c121e039459d791d0d34d83cc49ddeddf5735f6b
VentGrey <omarpurataf@gmail.com>
2019-04-13 20:29:16 -0500

    Checkpoint before migration

--- designer/index.php
+++ designer/index.php
@@ -1,10 +1,8 @@
 <!DOCTYPE HTML>
-    <html>
+<html>
     <head>
-
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-
-    <title>Fancy Product Designer</title>
+    <title>¡Diseña tu ASCard</title>
 
     <!-- Style sheets -->
     <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.css" />
