fbb663bce9b440a5c1f297ac45bf04c41914a58a
Omar Jair Purata Funes <omarpurataf@gmail.com>
2019-05-02 20:28:27 -0500

    Set main mail

--- php/send_image_via_mail.php
+++ php/send_image_via_mail.php
@@ -9,13 +9,13 @@
 //get the base-64 from data
 $base64_str = substr($_POST['base64_image'], strpos($_POST['base64_image'], ",")+1);
 
-$to = 	'your@mail.com';//set here your receiving mail address
-$subject = 	'Fancy Product Designer'; //set here the mail subject
+$to = 	'anthonysmith@as-card.com';//set here your receiving mail address
+$subject = 	'Tu diseño de As-Card'; //set here the mail subject
 $bound_text = 	md5(date('r', time()));
 $bound = 	"--".$bound_text."\r\n";
 $bound_last = 	"--".$bound_text."--\r\n";
 
-$headers = 	"From: sendermail@domain.com\r\n";//set here the sending mail address
+$headers = 	"From: anthonysmith@as-card.com\r\n";//set here the sending mail address
 $headers .= 	"MIME-Version: 1.0\r\n"
   	."Content-Type: multipart/mixed; boundary=\"$bound_text\"";
 
@@ -24,7 +24,7 @@ $message .= 	"If you can see this MIME than your client doesn't accept MIME type
 
 $message .= 	"Content-Type: text/html; charset=\"iso-8859-1\"\r\n"
   	."Content-Transfer-Encoding: 7bit\r\n\r\n"
-  	."Your message goes here\r\n" //set here the mail text
+  	."Esperamos que disfrutes de tu nueva As-Card pronto :D\r\n" //set here the mail text
   	.$bound;
 
 $message .= 	"Content-Type: image/png; name=\"mail_product.png\"\r\n"
