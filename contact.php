4929ec8c7e9bd37a834dbbe76fb53acde2bc9d1b
Omar Jair Purata Funes <omarpurataf@gmail.com>
2019-07-22 02:09:36 -0500

    Formatted page to make styling easier

--- contact.php
+++ contact.php
@@ -1,176 +1,155 @@
+<!DOCTYPE html>
 <html lang="es">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <title>As-Card</title>
+        <link rel="shortcut icon" href="img/favicon.ico">
+        <link rel="icon" type="image/gif" href="img/animated_favicon1.gif">
 
-    <title>As-Card</title>
-    <link rel="shortcut icon" href="img/favicon.ico">
-    <link rel="icon" type="image/gif" href="img/animated_favicon1.gif">
+        <!--Icons css-->
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
 
-    <!--Icons css-->
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css"
-      integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+        <!-- Bootstrap core CSS -->
+        <link href="vendor/bootstrap/css/bootstrap-material-design.min.css" rel="stylesheet">
 
-    <!-- Bootstrap core CSS -->
-    <link href="vendor/bootstrap/css/bootstrap-material-design.min.css" rel="stylesheet">
+        <!-- Custom fonts for this template -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Lora:400,400i,700,700i" rel="stylesheet">
 
-    <!-- Custom fonts for this template -->
-    <link
-      href="https://fonts.googleapis.com/css?family=Raleway:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i"
-      rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Lora:400,400i,700,700i" rel="stylesheet">
-
-    <!-- Custom styles for this template -->
-    <link href="css/business-casual.min.css" rel="stylesheet">
-  </head>
-
-  <body>
-    <h1 class="site-heading text-center text-white d-none d-lg-block">
-      <span class="site-heading-upper text-primary mb-3">Cambiando la forma de hacer Negocios</span>
-      <span class="site-heading-lower">As-Card</span>
-    </h1>
-
-
-    <!-- Navigation -->
-    <nav class="navbar navbar-expand-lg navbar-dark py-lg-4" id="mainNav" style="background-color: #4284DA">
-      <div class="container">
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive"
-          aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarResponsive">
-          <ul class="navbar-nav mx-auto">
-            <li class="nav-item active px-lg-4">
-              <a class="nav-link text-uppercase text-expanded" href="index.php">Inicio
-                <span class="sr-only">(current)</span>
-              </a>
-            </li>
-            <li class="nav-item px-lg-4">
-              <a class="nav-link text-uppercase text-expanded" href="about.php">Acerca de</a>
-            </li>
-            <li class="nav-item px-lg-4">
-              <a class="nav-link text-uppercase text-expanded" href="products.php">Paquetes</a>
-            </li>
-            <li class="nav-item px-lg-4">
-              <a class="nav-link text-uppercase text-expanded" href="contact.php">Contacto</a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
-    
-    
-
-    <section class="page-section cta">
-      
-      
-      <div>
-      <h2>Contáctanos en:</h2> <br><br>
-
-              <form name="contactform" method="post" action="send_form_email.php">
-                    <table width="450px">
-                    <tr>
-                     <td valign="top">
-                      <label for="first_name">Nombre de la empresa *</label>
-                     </td>
-                     <td valign="top">
-                      <input  type="text" name="first_name" maxlength="50" size="30">
-                     </td>
-                    </tr>
-                    <tr>
-                     <td valign="top">
-                      <label for="last_name">Nombre de contacto *</label>
-                     </td>
-                     <td valign="top">
-                      <input  type="text" name="last_name" maxlength="50" size="30">
-                     </td>
-                    </tr>
-                    <tr>
-                     <td valign="top">
-                      <label for="email">Correo *</label>
-                     </td>
-                     <td valign="top">
-                      <input  type="text" name="email" maxlength="80" size="30">
-                     </td>
-                    </tr>
-                    <tr>
-                     <td valign="top">
-                      <label for="telephone">Telefono de contacto (Opcional)</label>
-                     </td>
-                     <td valign="top">
-                      <input  type="text" name="telephone" maxlength="30" size="30">
-                     </td>
-                    </tr>
-                    <tr>
-                     <td valign="top">
-                      <label for="message">Mensaje *</label>
-                     </td>
-                     <td valign="top">
-                      <textarea  name="message" maxlength="1000" cols="25" rows="6"></textarea>
-                     </td>
-                    </tr>
-                    <tr>
-                     <td colspan="2" style="text-align:center">
-                      <input type="submit" value="Enviar">   <a href="http://as-card.com./email_form.php"></a>
-                     </td>
-                    </tr>
-                    </table>
-            </form> <br><br>
-    </div>
-      
-      
-      <div class="container">
-        <div class="row">
-          <div class="col-xl-9 mx-auto">
-            <div class="cta-inner text-center rounded">
-              <h2 class="section-heading mb-5">
-                <span class="section-heading-upper">Trabajamos para ti</span>
-              </h2>
-              
-              
-
-
-              <div class="intro-button mx-auto">
-
-              <h2>Conócenos en:</h2><br>
-              <h3>
-                 <a class="btn btn-info btn-lg btn-raised" href="https://www.facebook.com/as.card.mx/" target="_blank"><i class="fab fa-facebook"></i></a>
-                 <a class="btn btn-info btn-lg btn-raised" href="https://www.instagram.com/ascardmx/" target="_blank"><i class="fab fa-instagram"></i></a>
-                 <a class="btn btn-info btn-lg btn-raised" href="https://www.youtube.com/channel/UCc4CtnX0hwTuHUK3UaX5MQg?view_as=subscriber" target="_blank"><i
-                  class="fab fa-youtube"></i></a>
-              </h3>
-              </div>
-          </div>
+        <!-- Custom styles for this template -->
+        <link href="css/business-casual.min.css" rel="stylesheet">
+    </head>
+    <body>
+        <h1 class="site-heading text-center text-white d-none d-lg-block">
+            <span class="site-heading-upper text-primary mb-3">Cambiando la forma de hacer Negocios</span>
+            <span class="site-heading-lower">As-Card</span>
+        </h1>
+        <!-- Navigation -->
+        <nav class="navbar navbar-expand-lg navbar-dark py-lg-4" id="mainNav" style="background-color: #4284DA">
+            <div class="container">
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav mx-auto">
+                        <li class="nav-item active px-lg-4">
+                            <a class="nav-link text-uppercase text-expanded" href="index.php">Inicio
+                                <span class="sr-only">(current)</span>
+                            </a>
+                        </li>
+                        <li class="nav-item px-lg-4">
+                            <a class="nav-link text-uppercase text-expanded" href="about.php">Acerca de</a>
+                        </li>
+                        <li class="nav-item px-lg-4">
+                            <a class="nav-link text-uppercase text-expanded" href="products.php">Paquetes</a>
+                        </li>
+                        <li class="nav-item px-lg-4">
+                            <a class="nav-link text-uppercase text-expanded" href="contact.php">Contacto</a>
+                        </li>
+                    </ul>
+                </div>
             </div>
-          </div>
-        </div>
-      </div>
-    </section>
+        </nav>
+        <section class="page-section cta">
+            <div>
+                <h2>Contáctanos en:</h2> <br><br>
+                <div class="form-group">
+                    <form name="contactform" method="post" action="send_form_email.php">
+                        <table width="450px">
+                            <tr>
+                                <td valign="top">
+                                    <label for="first_name">Nombre de la empresa *</label>
+                                </td>
+                                <td valign="top">
+                                    <input  type="text" name="first_name" maxlength="50" size="30">
+                                </td>
+                            </tr>
+                            <tr>
+                                <td valign="top">
+                                    <label for="last_name">Nombre de contacto *</label>
+                                </td>
+                                <td valign="top">
+                                    <input  type="text" name="last_name" maxlength="50" size="30">
+                                </td>
+                            </tr>
+                            <tr>
+                                <td valign="top">
+                                    <label for="email">Correo *</label>
+                                </td>
+                                <td valign="top">
+                                    <input  type="text" name="email" maxlength="80" size="30">
+                                </td>
+                            </tr>
+                            <tr>
+                                <td valign="top">
+                                    <label for="telephone">Telefono de contacto (Opcional)</label>
+                                </td>
+                                <td valign="top">
+                                    <input  type="text" name="telephone" maxlength="30" size="30">
+                                </td>
+                            </tr>
+                            <tr>
+                                <td valign="top">
+                                    <label for="message">Mensaje *</label>
+                                </td>
+                                <td valign="top">
+                                    <textarea  name="message" maxlength="1000" cols="25" rows="6"></textarea>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td colspan="2" style="text-align:center">
+                                    <input type="submit" value="Enviar">   <a href="http://as-card.com./email_form.php"></a>
+                                </td>
+                            </tr>
+                        </table>
+                    </form> <br><br>
+                </div>
+            </div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-xl-9 mx-auto">
+                        <div class="cta-inner text-center rounded">
+                            <h2 class="section-heading mb-5">
+                                <span class="section-heading-upper">Trabajamos para ti</span>
+                            </h2>
+                            <div class="intro-button mx-auto">
+                                <h2>Conócenos en:</h2><br>
+                                <h3>
+                                    <a class="btn btn-info btn-lg btn-raised" href="https://www.facebook.com/as.card.mx/" target="_blank"><i class="fab fa-facebook"></i></a>
+                                    <a class="btn btn-info btn-lg btn-raised" href="https://www.instagram.com/ascardmx/" target="_blank"><i class="fab fa-instagram"></i></a>
+                                    <a class="btn btn-info btn-lg btn-raised" href="https://www.youtube.com/channel/UCc4CtnX0hwTuHUK3UaX5MQg?view_as=subscriber" target="_blank"><i
+                                                                                                                                                                                     class="fab fa-youtube"></i></a>
+                                </h3>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            </div>
+        </section>
+        <footer class="footer text-faded text-center py-5">
+            <div class="container">
+                <p class="m-0 small">Copyright &copy; As-Card 2019 </p>
+            </div>
+            <h3>
+                <a href="https://www.facebook.com/as.card.mx/" target="_blank"><i class="fab fa-facebook"></i></a>
+                <a href="https://www.instagram.com/ascardmx/" target="_blank"><i class="fab fa-instagram"></i>
+                </a>
+                <a href="https://www.youtube.com/channel/UCc4CtnX0hwTuHUK3UaX5MQg?view_as=subscriber" target="_blank"><i class="fab fa-youtube"></i></a>
+            </h3>
+        </footer>
 
-    <footer class="footer text-faded text-center py-5">
-      <div class="container">
-        <p class="m-0 small">Copyright &copy; As-Card 2019 </p>
-      </div>
-      <h3>
-        <a href="https://www.facebook.com/as.card.mx/" target="_blank"><i class="fab fa-facebook"></i></a>
-        <a href="https://www.instagram.com/ascardmx/" target="_blank"><i class="fab fa-instagram"></i>
-        </a>
-        <a href="https://www.youtube.com/channel/UCc4CtnX0hwTuHUK3UaX5MQg?view_as=subscriber" target="_blank"><i
-            class="fab fa-youtube"></i></a>
-      </h3>
-    </footer>
+        <!-- Bootstrap core JavaScript -->
+        <script src="vendor/jquery/jquery.min.js"></script>
+        <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+        <script src="vendor/bootstrap/js/bootstrap-material-design.min.js"></script>
+    </body>
 
-    <!-- Bootstrap core JavaScript -->
-    <script src="vendor/jquery/jquery.min.js"></script>
-    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-    <script src="vendor/bootstrap/js/bootstrap-material-design.min.js"></script>
-  </body>
-
-  <!-- Script to highlight the active date in the hours list -->
-  <script>
-    $('.list-hours li').eq(new Date().getDay()).addClass('today');
-  </script>
-
+    <!-- Script to highlight the active date in the hours list -->
+    <script>
+     $('.list-hours li').eq(new Date().getDay()).addClass('today');
+    </script>
 </html>
